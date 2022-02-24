052100ee0b751d05e26560c99998c71b92d918cf
handirachmawan <mail.handira@gmail.com>
2022-02-24 09:56:16 +0700

    Modify keys in user book table

--- model/user_book.go
+++ model/user_book.go
@@ -3,13 +3,13 @@ package model
 import "time"
 
 type UserBook struct {
-	UserBookID int       `gorm:"primaryKey;autoIncrement"`
-	UserID     int       `gorm:"primaryKey"`
-	BookID     int       `gorm:"primaryKey"`
-	PageRead   int       `gorm:"type:int(10);not null;default:0"`
-	IsFinish   bool      `gorm:"default:false;not null"`
-	CreatedAt  time.Time `gorm:"not null"`
-	UpdatedAt  time.Time `gorm:"not null"`
+	ID        int       `gorm:"primaryKey;autoIncrement"`
+	UserID    int       `gorm:"foreignKey"`
+	BookID    int       `gorm:"foreignKey"`
+	PageRead  int       `gorm:"type:int(10);not null;default:0"`
+	IsFinish  bool      `gorm:"default:false;not null"`
+	CreatedAt time.Time `gorm:"not null"`
+	UpdatedAt time.Time `gorm:"not null"`
 }
 
 func (UserBook) TableName() string {
