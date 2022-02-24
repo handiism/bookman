afe029b212f943be27f548f7a59650ddd468c085
handirachmawan <mail.handira@gmail.com>
2022-02-24 09:56:33 +0700

    Add user book repo

--- repo/user_book.go
+++ repo/user_book.go
@@ -6,8 +6,11 @@ import (
 )
 
 type UserBook interface {
-	FindAll() (map[string]interface{}, error)
 	Insert(model.UserBook) (model.UserBook, error)
+	FindById(int) (map[string]interface{}, error)
+	Update(model.UserBook) (model.UserBook, error)
+	Delete(model.UserBook) error
+	FindAll() ([]model.UserBook, error)
 }
 
 type userBook struct {
@@ -18,18 +21,6 @@ func NewUserBookImpl(db *gorm.DB) UserBook {
 	return &userBook{db}
 }
 
-func (repo *userBook) FindAll() (map[string]interface{}, error) {
-	var userBooks map[string]interface{}
-
-	err := repo.db.Model(model.UserBook{}).Take(&userBooks).Error
-
-	if err != nil {
-		return nil, err
-	}
-
-	return userBooks, nil
-}
-
 func (repo *userBook) Insert(userBook model.UserBook) (model.UserBook, error) {
 	err := repo.db.Create(&userBook).Error
 
@@ -38,4 +29,59 @@ func (repo *userBook) Insert(userBook model.UserBook) (model.UserBook, error) {
 	}
 
 	return userBook, nil
+}
+
+func (repo *userBook) FindById(id int) (map[string]interface{}, error) {
+	var userBook map[string]interface{}
+	err := repo.db.Raw(
+		`SELECT user_book.id,
+				user_book.user_id,
+				user_book.book_id,
+				user_book.page_read,
+				book.page_count,
+				user_book.is_finish,
+				user_book.created_at,
+				user_book.updated_at
+ 		 FROM user_book
+		 			 JOIN book ON user_book.book_id = book.id
+ 		 WHERE user_book.id = ?`, id,
+	).Scan(&userBook).Error
+
+	if err != nil {
+		return map[string]interface{}{}, err
+	}
+
+	return userBook, nil
+}
+
+func (repo *userBook) Update(userBook model.UserBook) (model.UserBook, error) {
+	err := repo.db.Save(&userBook).Error
+
+	if err != nil {
+		return model.UserBook{}, err
+	}
+
+	return userBook, nil
+}
+
+func (repo *userBook) Delete(book model.UserBook) error {
+	err := repo.db.Delete(book).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (repo *userBook) FindAll() ([]model.UserBook, error) {
+	var userBooks []model.UserBook
+
+	err := repo.db.Find(&userBooks).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return userBooks, nil
 }
